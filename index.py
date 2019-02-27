339eb20ca17720924d29298956698b968af7e030
chenlin <chenlin19970710@qq.com>
2019-02-27 17:54:35 +0800

    init

--- index.py
+++ index.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask,render_template,request,redirect,url_for,abort,jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello,world'
 
+@app.route('/')
+def hello_world():
+    return render_template('index.html',name='chenlin')
+
+# @app.route('/login',methods=['POST','GET'])
+# def login():
+#     error = None
+#     if request.method=='POST':
+#         if valid_login(request.form['username'],request.form['password']):
+#             return log_the_user_in(request.form['username'])
+#         else:
+#             error='Invalid username/password'
+#     return render_template('index.html',error=error)
+
+@app.route('/login')
+def login():
+        return redirect(url_for('error'))
+@app.route('/error')
+def error():
+    abort(401)
+    return render_template('index.html',name='error')
+
+@app.route('/one',methods=['GET'])
+def one():
+    return jsonify({"name":"chenlin"})
 if __name__ == '__main__':
-    app(debug=True)
+    app.run()
